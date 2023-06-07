ce44d0d3301e3a274f74e94ca559bf0f6dba5278
Marc Ryan <ryanmq@gmail.com>
2023-06-07 15:38:37 -0700

    Set text and background colors using tailwind classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className=" bg-white text-gray-800 dark:bg-gray-800 dark:text-whiteAlpha-900">
         <AppStateProvider>
           <AppBar />
           <Main>{props.children}</Main>
